6e47323c2eb31dce7ecb22ce6de6c6e5a16c1885
Odery <kammurimini@gmai.com>
2024-04-24 10:33:13 +0300

    added inline and reply buttons

--- internal/bot.go
+++ internal/bot.go
@@ -16,6 +16,19 @@ type TelegramBot struct {
 	tele.Bot
 }
 
+var (
+	// Universal markup builder
+	menu	 	= &tele.ReplyMarkup{ResizeKeyboard: true}
+	selector 	= &tele.ReplyMarkup{}
+
+	// Reply buttons
+	btnStatus	= menu.Text("Статус")
+	btnSettings	= menu.Text("Настройки")
+
+	// Inline buttons
+	btnStart	= selector.Data("Старт", "prev")
+	btnStop		= selector.Data("Стоп", "next")
+)
 
 // NewTelegramBot initializes and returns a new instance of TelegramBot.
 // This function creates a new Telegram bot instance using the token provided in the configuration,
@@ -36,10 +49,39 @@ func NewTelegramBot() *TelegramBot {
 	// Adding middleware to the logger
 	bot.Use(middleware.Whitelist(configs.TeleConfig.GetAdminID()))
 
+	// Adding Reply and Inline buttons
+	menu.Reply(
+		menu.Row(btnStatus),
+		menu.Row(btnSettings),
+	)
+
+	selector.Inline(
+		selector.Row(btnStart, btnStop),
+	)
+
 	// Adding handles to the bot
-	bot.Handle("/start", func(c tele.Context) error{
-		return c.Send("Initialization complete")
+	bot.Handle("/start", func(ctx tele.Context) error{
+		return ctx.Send("Initialization complete", menu)
 	})
+
+	// On reply button pressed handlers
+	bot.Handle(&btnStatus, func(ctx tele.Context) error {
+		return ctx.Send("Status is: in Development", selector)
+	})
+	bot.Handle(&btnSettings, func(ctx tele.Context) error {
+		return ctx.Send("in Development")
+	})
+
+	// On inline button pressed handlers
+	bot.Handle(&btnStart, func(ctx tele.Context) error {
+		ctx.Send("Спамер запущен")
+		return ctx.Respond()
+	})
+	bot.Handle(&btnStop, func(ctx tele.Context) error {
+		ctx.Send("Спамер остановлен")
+		return ctx.Respond()
+	})
+
 
 	return &TelegramBot{
 		Bot: *bot,
